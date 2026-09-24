b2ba8a79fe1616f649cbc27e1f5d89a857155c91
JulianMM-Griffith <julian.morris-mason@griffithuni.edu.au>
2026-09-25 01:28:32 +1000

    Following player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum EnemyState
+{
+    Patrolling,
+    Following
+}
+
 public class EnemyController : MonoBehaviour
 {
     [Header("References")]
+    [SerializeField] private Transform player;
     [SerializeField] private Transform[] patrolPoints;
 
     [Header("Settings")]
     [SerializeField] private float patrolWaitTime = 2f;
     [SerializeField] private float stopAtDistance = 0.5f;
+    [SerializeField] private float detectionRange = 5f;
+    [SerializeField] private float viewAngle = 90f;
+    [SerializeField] private float losePlayerTime = 3f;
 
     private NavMeshAgent agent;
     private Animator animator;
+    private EnemyState state = EnemyState.Patrolling;
     private int currentPatrolIndex;
     private bool isWaiting;
+    private float timeSinceLostPlayer;
 
     private void Awake()
     {
@@ -29,8 +41,43 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        Patrol();
+        var distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        switch (state)
+        {
+            case EnemyState.Patrolling:
+                Patrol();
+                if (distanceToPlayer <= detectionRange && CanSeePlayer())
+                {
+                    state = EnemyState.Following;
+                }
+
+                break;
+
+            case EnemyState.Following:
+                FollowPlayer();
+                if (!CanSeePlayer())
+                {
+                    timeSinceLostPlayer += Time.deltaTime;
+                    if (timeSinceLostPlayer >= losePlayerTime)
+                    {
+                        state = EnemyState.Patrolling;
+                        GoToClosestPatrolPoint();
+                    }
+                }
+                else
+                {
+                    timeSinceLostPlayer = 0f;
+                }
+                break;
+
+        }
         UpdateAnimations();
+    }
+
+    private void FollowPlayer()
+    {
+        agent.SetDestination(player.position);
     }
 
     private void Patrol()
@@ -61,6 +108,49 @@ public class EnemyController : MonoBehaviour
 
         agent.SetDestination(patrolPoints[currentPatrolIndex].position);
         currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+    }
+
+    private void GoToClosestPatrolPoint()
+    {
+        if (patrolPoints.Length == 0) return;
+        var closestIndex = 0;
+        var closestDistance = float.MaxValue;
+
+        for (var i = 0; i < patrolPoints.Length; i++)
+        {
+            var distance = Vector3.Distance(transform.position, patrolPoints[i].position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestIndex = i;
+            }
+        }
+
+        currentPatrolIndex = closestIndex;
+        agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+    }
+
+    private bool CanSeePlayer()
+    {
+        return IsFacingPlayer() && HasClearPathToPlayer();
+    }
+
+    private bool IsFacingPlayer()
+    {
+        var dirToPlayer = (player.position - transform.position).normalized;
+        var angle = Vector3.Angle(transform.forward, dirToPlayer); 
+        return angle <= viewAngle / 2f;
+    }
+
+    private bool HasClearPathToPlayer()
+    {
+        var dirToPlayer = player.position - transform.position;
+        if (Physics.Raycast(transform.position, dirToPlayer.normalized, out RaycastHit hit, dirToPlayer.magnitude))
+        {
+            return hit.transform == player;
+        }
+
+        return true;
     }
 
     private void UpdateAnimations()
